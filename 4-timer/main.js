ea9319acfc14dc7ca8480cc8c7f4f5a64d7b95b8
sergkryk <sergkryk@ya.ru>
2023-07-17 08:51:45 +0300

    adds 4-timer

---
+++ 4-timer/main.js
@@ -0,0 +1,52 @@
+function monthLeft() {
+  const MONTHS_IN_YEAR = 12;
+  const currentMonth = new Date().getMonth() + 1;
+  return MONTHS_IN_YEAR - currentMonth;
+}
+
+function daysLeft() {
+  const DAYS_IN_DECEMBER = 31;
+  const currentDay = new Date().getDate();
+  return DAYS_IN_DECEMBER - currentDay;
+}
+ 
+function timeLeft() {
+  const today = new Date();
+  const nextDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 1
+  );
+  const diff = nextDay.getTime() - today.getTime();
+
+  const hoursLeft = Math.floor(diff / 1000 / 60 / 60);
+  const minutesLeft = Math.floor((diff / 1000 / 60) % 60);
+  const secondsLeft = Math.floor((diff / 1000) % 60);
+
+  return `${hoursLeft} часов, ${minutesLeft} минут, ${secondsLeft} секунд.`
+}
+
+function updateCounter() {
+  return `${monthLeft()} месяцев, ${daysLeft()} дней, ${timeLeft()}`;
+}
+
+function renderTitle(title) {
+  const el = document.createElement("h1");
+  el.textContent = title;
+  return el;
+}
+
+function renderCounter() {
+  const el = document.createElement("p");
+  setInterval(() => {
+    el.textContent = updateCounter();
+  })
+  return el;
+}
+
+
+const app = document.querySelector('#app');
+app.appendChild(renderTitle("До Нового Года осталось:"));
+app.appendChild(renderCounter());
+
+console.log(timeLeft());
