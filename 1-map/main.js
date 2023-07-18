2cde010f19f7945ad533e2e7539f1b93ef3a1314
sergkryk <sergkryk@ya.ru>
2023-07-18 14:38:03 +0300

    adds object every key comparison

--- 1-map/main.js
+++ 1-map/main.js
@@ -5,6 +5,16 @@ const sampleArr = [
   { id: 3, name: "Антон" },
 ];
 
+const sample2 = [
+  { id: 1, name: "Семён" },
+  { id: 1, name: "Вася" },
+  { id: 2, name: "Петя" },
+  { id: 1, name: "Семён" },
+  { id: 3, name: "Антон" },
+  { id: 3, name: "Антон" },
+  { id: 3, name: "Даша" },
+];
+
 function getUniqueWithSet(arr) {
   return Array.from(new Set(arr.map((a) => a.id))).map((id) => {
     return arr.find((a) => a.id === id);
@@ -21,5 +31,38 @@ function getUniqueWithReduce(arr) {
   }, []);
 }
 
-console.log(getUniqueWithSet(sampleArr));
-console.log(getUniqueWithReduce(sampleArr));
+
+// проверка объектов на одинаковость сравнивая все ключи первого уровня
+function isShallowEqual(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// функция проверки массива объектов 
+function getUniqueObjects(arr) {
+  return arr.reduce((acc, el) => {
+    if (
+      acc.some((accEl) => {
+        return isShallowEqual(accEl, el);
+      })
+    ) {
+      return acc;
+    }
+    acc.push(el);
+    return acc;
+  }, []);
+}
+
+console.log(getUniqueObjects(sample2));
