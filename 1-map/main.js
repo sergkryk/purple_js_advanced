3e0e82ee3dfa5c849bdcdc6f5699a6a6a201d516
sergkryk <sergkryk@ya.ru>
2023-07-18 13:27:34 +0300

    adds functions

--- 1-map/main.js
+++ 1-map/main.js
@@ -1,22 +1,25 @@
-const arr = [
-  {id: 1, name: "Вася"},
-  {id: 2, name: "Петя"},
-  {id: 1, name: "Семён"},
-  {id: 3, name: "Антон"},
-]
+const sampleArr = [
+  { id: 1, name: "Вася" },
+  { id: 2, name: "Петя" },
+  { id: 1, name: "Семён" },
+  { id: 3, name: "Антон" },
+];
 
-const uniqueWithSet = Array.from(new Set(arr.map(a => a.id)))
- .map(id => {
-   return arr.find(a => a.id === id)
- });
+function getUniqueWithSet(arr) {
+  return Array.from(new Set(arr.map((a) => a.id))).map((id) => {
+    return arr.find((a) => a.id === id);
+  });
+}
 
-
- const uniqueWithReduce = arr.reduce((acc, el) => {
-  if (acc.some(item => item.id === el.id)) {
+function getUniqueWithReduce(arr) {
+  return arr.reduce((acc, el) => {
+    if (acc.some((item) => item.id === el.id)) {
+      return acc;
+    }
+    acc.push(el);
     return acc;
-  }
-  acc.push(el);
-  return acc;
- }, []);
+  }, []);
+}
 
- console.log(uniqueWithReduce);
+console.log(getUniqueWithSet(sampleArr));
+console.log(getUniqueWithReduce(sampleArr));
