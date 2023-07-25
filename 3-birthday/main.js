ee5545192647363506e43f5e002584ffddb2da88
sergkryk <sergkryk@ya.ru>
2023-07-25 08:43:09 +0300

    fix after review

--- 3-birthday/main.js
+++ 3-birthday/main.js
@@ -5,19 +5,6 @@ function isAdult(personBirthday, minAge = 14) {
   return today.getTime() > personIsAdultAt.getTime();
 }
 
-function isAdultV2(personBirthday, minAge = 14) {
-  const [year, month, date] = personBirthday.split("-");
-  const birthday = new Date(Number(year), Number(month) + 1, date);
-  const diff = new Date().getTime() - birthday.getTime();
-  return (new Date(diff).getUTCFullYear() - 1970) > minAge;
-}
-
-console.log(isAdult('2010-07-19'));
-console.log(isAdult('2002-10-20'));
-console.log(isAdult('1983-06-07'));
-
-console.log('break');
-
-console.log(isAdultV2('2010-07-19'));
-console.log(isAdultV2('2002-02-20'));
-console.log(isAdultV2('1983-06-07'));
+console.log(isAdult('2009-07-24'));
+console.log(isAdult('2009-07-25'));
+console.log(isAdult('2009-07-26'));
