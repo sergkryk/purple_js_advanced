cc17cba31eac052cb3ede2f794aa66cd5f6a8dc2
sergkryk <sergkryk@ya.ru>
2023-08-03 21:31:14 +0300

    fixes

--- 9-pokemon/main.js
+++ 9-pokemon/main.js
@@ -1,4 +1,11 @@
-const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+"use strict";
+
+const URL = "https://pokeapi.co/api/v2/pokemon";
+
+// интересно нужна ли такая функция ради функции?
+function getResponseJson(data) {
+  return JSON.parse(data);
+}
 
 function makeHttpRequest(url, method = "GET") {
   const request = new XMLHttpRequest();
@@ -7,21 +14,44 @@ function makeHttpRequest(url, method = "GET") {
   return request;
 }
 
-const pokemonRequest = makeHttpRequest(URL);
-
-function abilityResponseHandler() {
-  console.log(JSON.parse(this.responseText));
+function consoleData() {
+  console.log(getResponseJson(this.responseText));
 }
 
-function pokemonResponseHandler() {
-  const { abilities } = JSON.parse(this.responseText);
+function alertData() {
+  const { name } = getResponseJson(this.responseText);
+  alert(`The ability is ${name}`);
+}
+
+function doSomethingWithData() {
+  console.log(
+    `I am doing something with the ${getResponseJson(this.responseText).name}`
+  );
+}
+
+function getAbilityDetails(url, detailsResponseHandler) {
+  const abilityRequest = makeHttpRequest(url);
+  abilityRequest.addEventListener("load", detailsResponseHandler);
+}
+
+function pokemonResponseHandler (detailsResponseHandler) {
+  const { abilities } = getResponseJson(this.responseText);
   if (abilities.length && abilities.length > 0) {
-    const { ability } = abilities[0];
-    if ("url" in ability) {
-      const abilityRequest = makeHttpRequest(ability.url);
-      abilityRequest.addEventListener("load", abilityResponseHandler);
-    }
+    const [{ ability }] = abilities;
+    getAbilityDetails(ability?.url, detailsResponseHandler);
   }
+};
+
+function getPokemonAbility(name, detailsResponseHandler) {
+  const pokemonRequest = makeHttpRequest(`${URL}/${name}`);
+
+  pokemonRequest.addEventListener("load", function() {
+    // вот здесь не пойму, по идее если бы я обьявил pokemonResponseHandler через стрелочную функцию то мне бы не нужно было привязывать this,
+    // но нет, ничего не работало и this указывал на Window а не на XMLHttpRequest
+    pokemonResponseHandler.call(this,detailsResponseHandler);
+  });
 }
 
-pokemonRequest.addEventListener("load", pokemonResponseHandler);
+getPokemonAbility("ditto", consoleData);
+getPokemonAbility("pikachu", alertData);
+getPokemonAbility("torchic", doSomethingWithData);
