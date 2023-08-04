16426991b84e3f772a118c4e35e86800b9122215
sergkryk <sergkryk@ya.ru>
2023-08-04 14:22:17 +0300

    adds 11-geolocation

---
+++ 11-geolocation/main.js
@@ -0,0 +1,12 @@
+function myLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+const myLoc = myLocation()
+  .then((data) => console.log("Мои координаты", data))
+  .catch((error) => console.log("Произошла ошибка", error));
+
+
+console.log("Обработка запроса");
