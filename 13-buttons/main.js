1cf6c93cbdb74e372db3053ac8ec46950fb1b7e2
sergkryk <sergkryk@ya.ru>
2023-08-08 13:15:39 +0300

    adds 13-buttons

---
+++ 13-buttons/main.js
@@ -0,0 +1,28 @@
+const DEFAULT_TEXT = "Нажми меня";
+const PRESSED_TEXT = "Нажато";
+const buttonsParentNode = document.querySelector(".counter__buttons");
+const counterNode = document.querySelector(".counter__count");
+
+function updateButtonsText(target) {
+  for (const button of [...buttonsParentNode.children]) {
+    button.textContent = DEFAULT_TEXT;
+  }
+  target.textContent = PRESSED_TEXT;
+}
+
+function counterController() {
+  let counter = 0;
+  return function() {
+    counter = counter += 1;
+    return counter;
+  }
+}
+
+const updateCounter = counterController();
+
+function clickHandler(event) {
+  updateButtonsText(event.target);
+  counterNode.textContent = updateCounter();
+}
+
+buttonsParentNode.addEventListener("click", clickHandler);
