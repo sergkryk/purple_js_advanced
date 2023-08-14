0eee006424cd34deb2c7d7c220496963b5d5c8a5
sergkryk <sergkryk@ya.ru>
2023-08-14 08:42:53 +0300

    review fix

--- 12-promise-race/main.js
+++ 12-promise-race/main.js
@@ -1,23 +1,44 @@
-'use strict';
+"use strict";
 
-const mockUrl = 'https://dummyjson.com/products/'
-const mockProductsId = [1,2,3,4,5,6,7,8];
+const mockUrl = "https://dummyjson.com/products/";
+const mockProductsId = [1, 2, 3, 4, 5, 6, 7, 8,];
 
-async function getProduct(id) {
-  const res = await fetch(`${mockUrl}${id}`);
-  const data = await res.json();
-  return data;
+function getProduct(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`${mockUrl}${id}`)
+      .then((response) => response.json())
+      .then(resolve, reject)
+      .catch(reject);
+  });
 }
 
-async function race(promises) {
-  const result = await Promise.race(promises);
-  return result;
+// оригинальная функция из интернета, не понимаю зачем делать Promise.resolve если у нас и так список промисов у которых есть метод then
+// const race = function(promisesArray) {
+//   return new Promise((resolve, reject) => {
+//     promisesArray.forEach((promise) => {
+//       Promise.resolve(promise)
+//         .then(resolve, reject) // resolve, when any of the input promise resolves
+//         .catch(reject); // reject, when any of the input promise rejects
+//     });
+//   });
+// };
+
+function race(promisesArray) {
+  return new Promise((resolve, reject) => {
+    promisesArray.forEach((promise) => {
+      promise.then(resolve, reject).catch(reject);
+    });
+  });
+};
+
+function main() {
+  const promisesArr = mockProductsId.map((el) => getProduct(el));
+  console.log(promisesArr);
+  race(promisesArr)
+    .then((winner) => console.log(winner))
+    .catch((error) => console.log("Error!", error));
 }
 
-async function main() {
-  const promisesArr = mockProductsId.map(el => getProduct(el))
-  const winner = await race(promisesArr);
-  console.log(winner);
-}
+
 
 main();
